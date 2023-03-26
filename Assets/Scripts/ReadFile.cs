5ad54fd9d4105ad2af7fd934db6da8f81c94db34
Irina Gornovskaya <gornowsckaia.irina201372@gmail.com>
2023-03-26 19:12:02 +0300

    Add project, add async function

---
+++ Assets/Scripts/ReadFile.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using Task = System.Threading.Tasks.Task;
+
+public class ReadFile : MonoBehaviour
+{
+    private const string Path = @"C:\Users\Admin\Documents\Lessons\Dz14\Assets\Text.txt";
+
+    private static async Task ReadTextFromFile()
+    {
+        var text = await System.IO.File.ReadAllTextAsync(Path);
+        Debug.Log(text);
+    }
+
+    private async void Start()
+    {
+        await ReadTextFromFile();
+    }
+}
